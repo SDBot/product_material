9a8474641981b3de8fb8e442f601cda6b7d85dc7
T.C <62317029+tchen-anergy@users.noreply.github.com>
2021-12-27 17:42:52 +0700

    Update manifest

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name' : 'Product Material',
-    'description':"""
+    'name': 'Product Material',
+    'author': "Tony",
+    'description': """
 This module allow customization of product materials
     """,
-    'version' : '1.0',
+    'version': '1.0',
     'category': 'Sales/Sales',
-    'depends' : ['base'],
+    'depends': ['base'],
     'data': [],
     'installable': True,
-    'application': False,
-    'author': "Anergy Pte Ltd",
+    'application': True,
 }
